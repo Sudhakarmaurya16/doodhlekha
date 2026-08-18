9cce5ff1e126b04f1bbec19cc6890011fb14a815
Sudhakar Maurya <sudhakar@gmail.com>
2026-08-18 12:42:20 +0530

    Fix AuthLoading CSS path

--- doodhlekha/src/components/ProtectedRoute.jsx
+++ doodhlekha/src/components/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import "../pages/auth/AuthLoading.css";
+import "../pages/Auth/AuthLoading.css";
 
 const ProtectedRoute = ({ children }) => {
   const { user, loading } = useAuth();
